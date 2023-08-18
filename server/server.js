07bfd7865cdb3910c7f7c174f20d94b5e18b0a05
Jay <wmeng013@gmail.com>
2023-08-18 16:17:09 +1000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Expose-Headers', 'Authorization');
     next();
 });
 
