fafa3bb5de3138500b09e95404076576c3d79d09
jc <139629197+jctech0412@users.noreply.github.com>
2023-08-18 14:25:41 +0800

    add
    add

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ require('./config/passport')(passport);
 const app = express();
 
 app.use(cors());
+app.use((express.json({ limit: "30mb", extended: true })));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
